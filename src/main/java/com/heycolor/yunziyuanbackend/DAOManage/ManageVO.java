1a7553941c14ce7f9fe41c6a961a827ec8d8ed9d
Lyang <1308717409@qq.com>
2025-03-30 01:22:16 +0800

    管理接口

---
+++ src/main/java/com/heycolor/yunziyuanbackend/DAOManage/ManageVO.java
@@ -0,0 +1,43 @@
+package com.heycolor.yunziyuanbackend.DAOManage;
+
+public class ManageVO {
+    private String user_number;
+
+    private String user_psw;
+
+    private String user_type;
+
+    private String operate;
+
+    public String getUser_number() {
+        return user_number;
+    }
+
+    public void setUser_number(String user_number) {
+        this.user_number = user_number;
+    }
+
+    public String getUser_psw() {
+        return user_psw;
+    }
+
+    public void setUser_psw(String user_psw) {
+        this.user_psw = user_psw;
+    }
+
+    public String getUser_type() {
+        return user_type;
+    }
+
+    public void setUser_type(String user_type) {
+        this.user_type = user_type;
+    }
+
+    public String getOperate() {
+        return operate;
+    }
+
+    public void setOperate(String operate) {
+        this.operate = operate;
+    }
+}
---
+++ src/main/java/com/heycolor/yunziyuanbackend/DAOManage/ResourceRe.java
@@ -0,0 +1,22 @@
+package com.heycolor.yunziyuanbackend.DAOManage;
+
+import java.util.Date;
+
+public class ResourceRe {
+    private Integer id;
+    private String data_title;
+    private Integer data_state;
+    private String data_img_list;
+    private String data_img_mx;
+    private String data_url;
+    private Integer data_good;
+    private String data_dir;
+    private String data_type;
+    private String data_country;
+    private String data_year;
+    private String data_note;
+    private Integer count_people;
+    private Integer count_score;
+    private Date gmt_create;
+
+}
